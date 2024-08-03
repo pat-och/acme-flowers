7b256b3d76d79a392e5d9b3cb0158197f0e6717f
Patrick Maige <patrickmaige@hotmail.com>
2024-08-03 13:23:09 +0200

    Add uuid to article
    & delete useless content field

--- src/Controller/ArticleListController.php
+++ src/Controller/ArticleListController.php
@@ -14,31 +14,27 @@ class ArticleListController extends AbstractController
         return $this->render('article/list.html.twig', [
             'articles' => [
                 [
-                    'id' => 1,
+                    'id' => 'abc',
                     'source' => 'src-1',
                     'title' => 'Article 1',
-                    'content' => 'Lorem ipsum dolor sit amet 1',
                     'publishedAt' => '2024-08-' . rand(26, 31)
                 ],
                 [
-                    'id' => 2,
+                    'id' => 'def',
                     'source' => 'src-2',
                     'title' => 'Article 2',
-                    'content' => 'Lorem ipsum dolor sit amet 2',
                     'publishedAt' => '2024-08-' . rand(20, 25)
                 ],
                 [
-                    'id' => 3,
+                    'id' => 'ghi',
                     'source' => 'src-2',
                     'title' => 'Article 3',
-                    'content' => 'Lorem ipsum dolor sit amet 3',
                     'publishedAt' => '2024-08-' . rand(10, 19)
                 ],
                 [
-                    'id' => 4,
+                    'id' => 'jkl',
                     'source' => 'src-1',
                     'title' => 'Article 4',
-                    'content' => 'Lorem ipsum dolor sit amet 4',
                     'publishedAt' => '2024-08-' . rand(1, 9)
                 ]
             ],
