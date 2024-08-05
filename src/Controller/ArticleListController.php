52a02d7e2392ba88d2c14aaea5e080b107a1d5c1
Patrick Maige <patrickmaige@hotmail.com>
2024-08-05 17:55:56 +0200

    Add new articles fixtures

--- src/Controller/ArticleListController.php
+++ src/Controller/ArticleListController.php
@@ -5,39 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Uid\Uuid;
 
 class ArticleListController extends AbstractController
 {
     #[Route('/articles', name: 'app_article_list')]
     public function index(): Response
     {
+
+        $articles = [];
+
+        for ($i = 1; $i <= 20; $i++) {
+            $articles[] =                 [
+                'id' => Uuid::v4(),
+                'source' => 'src-' . rand(1 , 2),
+                'title' => 'Article ' . $i,
+                'publishedAt' => '2024-0' . rand(0 , 9)  . '-' . rand(10 , 30),
+            ];
+        }
+
         return $this->render('article/list.html.twig', [
-            'articles' => [
-                [
-                    'id' => 'abc',
-                    'source' => 'src-1',
-                    'title' => 'Article 1',
-                    'publishedAt' => '2024-08-28',
-                ],
-                [
-                    'id' => 'def',
-                    'source' => 'src-2',
-                    'title' => 'Article 2',
-                    'publishedAt' => '2024-08-21',
-                ],
-                [
-                    'id' => 'ghi',
-                    'source' => 'src-2',
-                    'title' => 'Article 3',
-                    'publishedAt' => '2024-08-15',
-                ],
-                [
-                    'id' => 'jkl',
-                    'source' => 'src-1',
-                    'title' => 'Article 4',
-                    'publishedAt' => '2024-08-02',
-                ]
-            ],
+            'articles' => $articles
         ]);
     }
 }
